13afee7f272af4679d7cb9c9c3cace059704ba08
Sagar Adval <sagaradval@gmail.com>
2021-05-21 19:01:49 +0530

    Added socketio connection and script

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const PORT = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(PORT, () => {
+io.on("connection", () => {
+  console.log("New web socket connection!");
+});
+
+server.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
